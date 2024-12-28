8a679afbdf87b378af99c647d14a26729e722262
gitesh <giteshkumar99@gmail.com>
2024-12-28 20:46:57 +0530

    commit to remove

--- RatingService/src/main/java/com/lcwd/rating/RatingServiceApplication.java
+++ RatingService/src/main/java/com/lcwd/rating/RatingServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("removed");
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
 
